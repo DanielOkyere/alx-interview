e06892a53e226099feaac1112111c9927e7f355b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-04-06 13:17:52 +0000

    pycodestyles done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Prime game """
 
+
 def isWinner(x, nums):
     """Determines the winner for x rounds"""
     if x < 1 or not nums:
         return None
     m_win, n_win = 0, 0
     n = max(nums)
-    primes =[True for _ in range(1, n+1, 1)]
+    primes = [True for _ in range(1, n+1, 1)]
     primes[0] = False
     for i, is_prime in enumerate(primes, 1):
         if i == 1 or not is_prime:
